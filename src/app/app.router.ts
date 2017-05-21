186052600f32b7abbfff589754ccbcec2b8668fb
Thiago <thiagopereira2634@gmail.com>
2017-05-21 19:40:10 -0300

    update routers

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,14 +2,12 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './components/main/main.component';
 import { BooksComponent } from './components/books/books.component';
 import { BookInfoComponent } from './components/book-info/book-info.component';
 import { FavoritesBooksComponent } from './components/favorites-books/favorites-books.component';
 
 export const router: Routes = [
   { path: "", redirectTo: "book", pathMatch: "full"},
-  { path: "index", component: MainComponent},
   { path: "book", component: BooksComponent},
   { path: "favorites", component: FavoritesBooksComponent},
   { path: "bookInfo", component: BookInfoComponent},
